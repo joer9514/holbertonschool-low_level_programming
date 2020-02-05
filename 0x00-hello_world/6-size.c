33f6d66a950bba4db1e555f28a8d7fcd62fbdbba
joer9514 <1630@holbertonschool.com>
2020-02-05 15:45:57 -0500

    Size variables

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+*main - Output size variable
+*Description: Output variable size
+*Return: void
+*/
+int main(void)
+{
+char ch;
+int in;
+long int lon;
+long long int lli;
+float fl;
+printf("Size of a char: %ld byte(s)\n", sizeof(ch));
+printf("Size of an int: %ld byte(s)\n", sizeof(in));
+printf("Size of a long int: %ld byte(s)\n", sizeof(in));
+printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
+printf("Size of a float: %ld byte(s)\n", sizeof(fl));
+}
