7ebd491057c2fc8ec284df36735bc6175d7b4e23
joer9514 <1630@holbertonschool.com>
2020-02-05 15:07:03 -0500

    Use fuction printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*main - printf line
+*Description: Output with printf
+*Return: void
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
