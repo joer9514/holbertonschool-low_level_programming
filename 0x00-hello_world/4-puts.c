6388d9ed9763f5b84b19a3dde763a8729a4666af
joer9514 <1630@holbertonschool.com>
2020-02-05 14:36:00 -0500

    puts 1.0

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+/*Use fuction puts*/
 int main(void)
 {
-puts("\"..Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
