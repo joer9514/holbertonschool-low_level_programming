8827acf70255b1984f84760cd2bb9725f622d7b3
joer9514 <1630@holbertonschool.com>
2020-02-05 15:47:29 -0500

    Size variables..

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,4 +16,5 @@ printf("Size of an int: %ld byte(s)\n", sizeof(in));
 printf("Size of a long int: %ld byte(s)\n", sizeof(in));
 printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
 printf("Size of a float: %ld byte(s)\n", sizeof(fl));
+return (0);
 }
