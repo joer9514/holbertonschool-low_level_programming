2af0d215c5839488a959a56f19965b1aec7cc178
joer9514 <1630@holbertonschool.com>
2020-02-05 14:56:05 -0500

    puts 1.2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-/*Use fuction puts*/
+/**
+*main - puts line to stdout
+*Description: uses puts fuction
+*Return: void
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
