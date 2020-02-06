d69ac8f9497cde0e1ea1349518e53b71f155d1d5
joer9514 <1630@holbertonschool.com>
2020-02-06 15:40:08 -0500

    Update 2

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,29 +2,29 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - part two conditional
- * Description: part two conditional if
- * Return: 0
+* main - part two conditional
+* Description: part two conditional if
+* Return: 0
 */
 int main(void)
 {
 int n;
-
+int module;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 /* your code goes there */
-int modulo = n % 10;
-if (modulo > 5)
+module = n % 10;
+if (module > 5)
 {
-printf("Last digit of %d is %d and is greater than 5\n", n, modulo);
+printf("Last digit of %d is %d and is greater than 5\n", n, module);
 }
-else if (modulo == 0)
+else if (module == 0)
 {
-printf("Last digit of %d is %d and is 0\n", n, modulo);
+printf("Last digit of %d is %d and is 0\n", n, module);
 }
 else
 {
-printf("Last digit of %d is %d and is less than 6 and not 0\n", n, modulo);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, module);
 }
 return (0);
 }
