5e8fd1ce8bf0ba71c7966eabecb81d78f83221b0
joer9514 <1630@holbertonschool.com>
2020-02-06 13:52:09 -0500

    Conditional if

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+*main - Conditional if, ramdom
+*Description: This file is for validate if is negative or positive
+*Return: void
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n < 0)
+	{
+	printf("%d is negative\n", n);
+	}
+	else if (n > 0)
+	{
+	printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+	printf("%d is zero\n", n);
+	}
+	return (0);
+}
