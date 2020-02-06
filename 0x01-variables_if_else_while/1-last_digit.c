bb6c358bbbc34eff518bd8e8183188b8f70790fe
joer9514 <1630@holbertonschool.com>
2020-02-06 15:12:14 -0500

    less or greater

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - part 2 conditional
+ * Description: part 2 conditional if
+ * Return: 0
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	int modulo = n % 10;
+	if (modulo > 5)
+	{
+	printf("Last digit of %d is %d and is greater than 5\n", n, modulo);
+	}
+	else if (modulo < 6)
+	{
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, modulo);
+	}
+	else if (modulo == 0)
+	{
+	printf("Last digit of %d is %d and is 0\n", n, modulo);
+	}
+	return (0);
+}
