48aab914ce832326186a8f5a2df6b18d95ae3862
joer9514 <1630@holbertonschool.com>
2020-02-06 17:05:28 -0500

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - part three
+*Description: Lenguage c part three
+*Return: void
+*/
+int main(void)
+{
+char l = 'a';
+while (l <= 'z')
+{
+putchar(l);
+l++;
+}
+putchar('\n');
+return (0);
+}
