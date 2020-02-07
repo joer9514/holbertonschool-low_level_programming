7aa65c99c799783cceb7d41bee8f527335fdbf2e
joer9514 <1630@holbertonschool.com>
2020-02-06 19:26:15 -0500

    update...

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main - number
+*Description: number
+*Return: void
+*/
+int main(void)
+{
+int n;
+for (n = 0; n < 10; n++)
+{
+printf("%d", n);
+}
+putchar('\n');
+return (0);
+}
