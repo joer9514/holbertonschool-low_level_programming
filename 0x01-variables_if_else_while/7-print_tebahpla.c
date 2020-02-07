3be496b64ef8b10285d81fe7dadf09956f7e4cda
joer9514 <1630@holbertonschool.com>
2020-02-06 19:37:29 -0500

    reverse alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - part three
+*Description: Lenguage c part three
+*Return: void
+*/
+int main(void)
+{
+char l = 'z';
+while (l >= 'a')
+{
+putchar(l);
+l--;
+}
+putchar('\n');
+return (0);
+}
