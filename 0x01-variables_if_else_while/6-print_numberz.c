27f71df0e9e00d4de7df0b1187342e3e99007c81
joer9514 <1630@holbertonschool.com>
2020-02-06 19:32:35 -0500

    putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main - number 1
+*Description: number 1
+*Return: void
+*/
+int main(void)
+{
+int n;
+for (n = 0; n < 10; n++)
+{
+putchar(n + '0');
+}
+putchar('\n');
+return (0);
+}
