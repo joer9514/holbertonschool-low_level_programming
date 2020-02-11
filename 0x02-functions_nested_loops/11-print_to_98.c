3e891624feb1362d13c9baf2e0a17de550c88236
joer9514 <1630@holbertonschool.com>
2020-02-11 14:39:58 -0500

    print_98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+*print_to_98 - Message
+*@n: tables
+*Return: void
+*/
+void print_to_98(int n)
+{
+if (n < 99)
+{
+while (n <= 98)
+{
+printf("%d", n);
+if (n != 98)
+{
+printf(",");
+}
+if (n == 98)
+{
+printf("\n");
+}
+n++;
+}
+}
+else if (n > 98)
+{
+printf("%d", n);
+if (n != 98)
+{
+printf(",");
+}
+}
+}
