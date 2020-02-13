c387c11db2f43d637036b83da025633a8442041a
joer9514 <1630@holbertonschool.com>
2020-02-13 18:02:27 +0000

    First exercise

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+  * _isupper - First file
+  * @c: var
+  * Return: 0
+  */
+int _isupper(int c)
+{
+	if (c == 'A')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
