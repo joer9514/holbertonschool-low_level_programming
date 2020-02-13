476760e5a2cef12c3d84f1954cc4ec31e3236e5d
joer9514 <1630@holbertonschool.com>
2020-02-13 18:21:35 +0000

    UpperCase or not

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,17 +2,9 @@
 /**
   * _isupper - First file
   * @c: var
-  * Return: 1 - 0
+  * Return: 0
   */
 int _isupper(int c)
 {
-	if (c == 'A')
-	{
-		return (1);
-	}
-	else if (c == 'a')
-	{
-		return (0);
-	}
-	return (0);
+	return (c >= 'A' && c <= 'Z');
 }
