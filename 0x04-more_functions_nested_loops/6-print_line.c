75cf4f10f76d2c91d254f315bea7719184407f8a
joer9514 <1630@holbertonschool.com>
2020-02-13 21:29:54 +0000

    print lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+  * print_line - six exercice
+  * @n: var
+  * Return: void
+  */
+void print_line(int n)
+{
+	while (n-- > 0)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
