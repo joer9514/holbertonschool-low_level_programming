6a7f0232e2bbc060aabb4944de7439eab4dfa566
joer9514 <1630@holbertonschool.com>
2020-02-22 04:51:23 +0000

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,11 +8,15 @@
   */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i;
+	int i, j = 0;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[i] = src[i];
-	for (; i < n; i++)
-		dest[i] = '\0';
+	for (i = 0; *(dest + i) != '\0'; i++)
+	{
+	}
+	for (; j < n && *(src + j) != '\0'; i++, j++)
+	{
+		*(dest + i) = *(src + j);
+	}
+	*(dest + i) = '\0';
 	return (dest);
 }
