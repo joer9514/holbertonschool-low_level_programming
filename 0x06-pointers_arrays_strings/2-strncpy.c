9db11b37c740cf2f5370a0a3303cb392546a8236
joer9514 <1630@holbertonschool.com>
2020-02-22 04:54:53 +0000

    _strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * _strncpy - copies a string
+ * @dest: point1
+ * @src: point2
+ * @n: number
+ * Return: 0
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+
+	return (dest);
+}
