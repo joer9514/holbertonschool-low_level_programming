dbe1f6ebdbab3ba249db96fcbea0a6070bcaf580
joer9514 <1630@holbertonschool.com>
2020-02-24 15:43:02 +0000

    add update

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,9 +4,15 @@
  * @s: var1
  * @b: var2
  * @n: var3
- * Return: 0
+ * Return: char
  */
 char *_memset(char *s, char b, unsigned int n)
 {
+	unsigned int i;
 
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
 }
