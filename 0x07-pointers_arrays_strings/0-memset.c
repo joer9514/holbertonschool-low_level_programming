1a3c4f4a08f71eddb69fa47e491fec8167444895
joer9514 <1630@holbertonschool.com>
2020-02-24 15:33:23 +0000

    files readme, library and exercise

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * _memset - exercise 1
+ * @s: var1
+ * @b: var2
+ * @n: var3
+ * Return: 0
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+this project C part 7
