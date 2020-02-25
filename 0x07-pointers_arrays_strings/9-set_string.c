20f214c2da2de77153860342aa1e5915482f24c3
joer9514 <1630@holbertonschool.com>
2020-02-25 02:05:19 +0000

    exercise 10

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * set_string - sets value of a pointer to a char
+ * @s: var1
+ * @to: var2
+ * Return: void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
