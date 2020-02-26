83b640c60de082e1baf1a6aaf00f348b9db3517f
joer9514 <1630@holbertonschool.com>
2020-02-26 19:52:07 +0000

    exercise 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * _pow_recursion - exercise 3
+ * Description: exercise 4
+ * @x: var1
+ * @y: var2
+ * Return: int
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y > 0)
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+	return (1);
+}
