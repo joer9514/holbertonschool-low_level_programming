7c566ac63593964d43d5f9d9fb6f47e310132d18
joer9514 <1630@holbertonschool.com>
2020-03-02 02:39:55 +0000

    exercise 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - exercise 0
+ * Description: Return name
+ * @argc: argument length
+ * @argv: contains arguments
+ * Return: int
+ */
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
