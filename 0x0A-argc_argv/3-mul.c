1abe66bfe5653185c7bf097cbfea1460e052094d
joer9514 <1630@holbertonschool.com>
2020-03-02 04:20:05 +0000

    exercise 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - exercise 3
+ * Description: return mult int
+ * @argc: # de argumentos
+ * @argv: argumentos
+ * Return: int
+ */
+int  main(int argc, char __attribute__ ((unused)) *argv[])
+{
+
+	if (argc == 3)
+	{
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (1);
+}
