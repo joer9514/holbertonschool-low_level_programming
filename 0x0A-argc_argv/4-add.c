3dfdd3790384e7d07323e6fd6c24189c4bd1f787
joer9514 <1630@holbertonschool.com>
2020-03-02 04:31:14 +0000

    exercise 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - exercise 4
+ * @argc : argument
+ * @argv : argumetn
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	int i, acu = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		if (!(*argv[i] >= '0' && *argv[i] <= '9'))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+			acu += atoi(argv[i]);
+	}
+	printf("%d\n", acu);
+	return (0);
+}
