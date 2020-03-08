7f0a65ee46082d6cd24354edf731bcc588e9d40c
joer9514 <1630@holbertonschool.com>
2020-03-08 17:37:45 +0000

    advanced

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,18 +1,11 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
-* main - advance exercise
-* Description: return 1
-* Return: 1
-*/
+ * main - advance exercise
+ * Description: return 1
+ * Return: 1
+ */
 int main(void)
 {	
-	char phrase[60] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-	int i;
-
-	for (i = 0; i <= phrase[i]; i++)
-	{
-		putchar(phrase[i]);
-	}
-	putchar('\n');
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
