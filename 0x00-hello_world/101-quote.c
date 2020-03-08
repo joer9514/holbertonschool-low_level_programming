47e36c1cdd152a4c4ffa55603f282237b60deeab
joer9514 <1630@holbertonschool.com>
2020-03-08 17:21:49 +0000

    advance

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - advance exercise
+* Description: return 1
+* Return: 1
+*/
+int main(void)
+{	
+	char phrase[60] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	int i;
+
+	for (i = 0; i <= phrase[i]; i++)
+	{
+		putchar(phrase[i]);
+	}
+	putchar('\n');
+	return (1);
+}
