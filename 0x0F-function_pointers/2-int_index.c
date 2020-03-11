4fdc0a1db67e1262a9d9054345e76503315b557b
joer9514 <1630@holbertonschool.com>
2020-03-11 21:54:09 +0000

    commit

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,16 +10,16 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if ((array == NULL) || (cmp = NULL))
+	if ((array == NULL) && (cmp == NULL))
 	{
-		return (- 1);
-		for (i = 0; i < size; i++)
+		return (-1);
+	}
+	for (i = 0; i < size; i++)
+	{
+		if ((*cmp) (array[i]))
 		{
-			if ((*cmp) (array[i]))
-			{
-				return (i);
-			}
+			return (i);
 		}
 	}
-	return (- 1);
+	return (-1);
 }
