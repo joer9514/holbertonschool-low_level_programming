303a8596d2abf062ac6c3c0bc73f1d3705c6eb85
joer9514 <1630@holbertonschool.com>
2020-03-20 00:31:25 +0000

    update

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * add_node - new node
+ * @head: doble pointer
+ * @str: pointer
+ * Return: elements
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+
+	new_node = malloc(sizeof(list_t));
+
+	if (new_node == NULL)
+		return (NULL);
+	new_node->len = _strlen(str);
+	new_node->str = strdup(str);
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
+}
