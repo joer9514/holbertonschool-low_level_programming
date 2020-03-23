4bbe02bb48d187895beec283e1866ab2c64226a4
joer9514 <1630@holbertonschool.com>
2020-03-23 21:34:14 +0000

    Up exercises files

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+  * add_nodeint - function that adds a new node at the beginning
+  * @head: double pointer
+  * @n: constan n
+  * Return: the address of the new element, or NULL if it failed
+  */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+
+}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,6 +1,10 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+/*Library*/
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -16,6 +20,7 @@ typedef struct listint_s
 } listint_t;
 
 /*Prototype*/
+int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
