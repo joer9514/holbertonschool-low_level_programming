8cf7beed041e6dec12b1934b3af34d01164929b3
joer9514 <1630@holbertonschool.com>
2020-03-27 02:58:04 +0000

    exercise 2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * get_bit - function that returns the value of a bit.
+ * @n: the integer
+ * @index: unsigned int
+ * Return: the value of the bit at index index or -1 if an error occured
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= sizeof(n) * 8)
+		return (-1);
+	return (1 & (n >> index));
+}
