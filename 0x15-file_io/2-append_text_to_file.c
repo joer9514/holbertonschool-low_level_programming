0949ea78be398ce31625b624e65a22f977c7064c
joer9514 <1630@holbertonschool.com>
2020-04-01 04:14:17 +0000

    commit s

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -21,7 +21,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	{
 		txt_len++;
 	}
-	file_d = open(filename, O_WRONLY | O_APPEND;
+	file_d = open(filename, O_WRONLY | O_APPEND);
 
 	if (file_d == -1)
 		return (-1);
