13eac1800f87dcc10ddf3a8aff06455c1a1403d6
joer9514 <1630@holbertonschool.com>
2020-04-01 00:29:53 +0000

    first commit

---
+++ 0x15-file_io/holberton.h
@@ -0,0 +1,14 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/*Library*/
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+/*Prototype*/
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
+#endif
