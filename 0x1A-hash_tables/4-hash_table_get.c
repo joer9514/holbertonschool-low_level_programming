15a117f3c438e28835e0ffa8232b52b47a686dec
joer9514 <joer9514@gmail.com>
2020-05-29 19:06:07 -0500

    task 4 update

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,39 +1,27 @@
 #include "hash_tables.h"
-#include <stdio.h>
 /**
- * grab_value - return value string associated whit a key
- * @hn: pointer to hash node
- * @key: key for search
- * Return: value string, or NULL is no match
+ * hash_table_get - function
+ * @ht: hash table
+ * @key: key
+ * Return: value NULL
  */
-
-const char *grab_value(hash_node_t *hn, const char *key)
-{
-
-	while (hn != NULL)
-	{
-		if (strcmp(hn->key, key) == 0)
-		{
-			return (hn->value);
-		}
-		hn = hn->next;
-	}
-	return (NULL);
-}
-
-/**
- * hash_table_get - retrieve value associated with a key
- * @ht: pointer to a hash table
- * @key: key string
- * Return: value string
- */
-
-
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
+	unsigned long int idx = 0;
+	hash_node_t *tmp = NULL;
 
-	unsigned long int idx;
-
-	idx = key_index((const unsigned char *)key, ht->size);
-	return ((char *)(grab_value((ht->array[idx]), key)));
+	if (key == NULL)
+		return (NULL);
+	if (ht != NULL)
+	{
+		idx = key_index((const unsigned char *)key, ht->size);
+		tmp = ht->array[idx];
+		while (tmp != NULL)
+		{
+			if (strcmp(tmp->key, key) == 0)
+				return (tmp->value);
+			tmp = tmp->next;
+		}
+	}
+	return (NULL);
 }
